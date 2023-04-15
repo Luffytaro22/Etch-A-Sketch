e1afed896fec893c5b789347f6cc0742b3d9b635
Luffytaro22 <manuelsanchez059@gmail.com>
2023-04-15 15:01:59 -0500

    Added input that receives the user's number and create the divs

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,41 +1,55 @@
-let size = prompt("Choose a side, maximun 100");
-
-if(size <= 100){
-    for (let i = 1; i <= size*size; i++) {
-        let div = document.createElement('div');
-        var currentDiv = document.getElementById("container");
-        div.classList.add('grids');
-        container.appendChild(div);
-      }
-}else{
-    alert("ERROR");
+const input = document.querySelector('input');
+const container = document.querySelector('#container');
+let inputValue = 0;
+//Crea los divs dentro del contenedor.
+function createDivs(inputValue) {
+  for(let i = 1; i < inputValue*inputValue; i++){
+    const div = document.createElement('div');
+    div.classList.add('grids');
+    container.appendChild(div);
+  }
 }
 
+input.addEventListener('change', () => {
+  let aux = inputValue;
+  inputValue = input.valueAsNumber;
+  createDivs(inputValue);
+  
+
+  container.style.gridTemplateColumns = `repeat(${inputValue}, ${500/inputValue}px)`;
+  container.style.gridAutoRows = `minmax(${100/inputValue}px, auto)`;
+
   const trails = document.querySelectorAll(".grids");
+
   let mouseDown = false; // variable para indicar si el mouse está presionado
 
-// evento que activa el cambio de color para todos los elementos
-document.addEventListener('mousedown', () => {
-  mouseDown = true;
-});
-
-// evento que detiene el cambio de color para todos los elementos
-document.addEventListener('mouseup', () => {
-  mouseDown = false;
-});
-
-// evento que activa el cambio de color solo para el elemento actual
-trails.forEach((div) => {
-  div.addEventListener('mouseover', () => {
-    if (mouseDown) {
-      div.classList.add('clicked');
-    }
+  // evento que activa el cambio de color para todos los elementos
+  document.addEventListener('mousedown', () => {
+    mouseDown = true;
   });
+
+  // evento que detiene el cambio de color para todos los elementos
+  document.addEventListener('mouseup', () => {
+    mouseDown = false;
+  });
+
+  if(inputValue == aux){
+
+  }else{
+    trails.forEach((div) => {
+      div.addEventListener('mouseover', () => {
+        if (mouseDown) {
+          div.classList.add('clicked');
+        }
+      });
+    });
+  }
+  // evento que activa el cambio de color solo para el elemento actual
+  
+
+  
+
 });
-
-container.style.gridTemplateColumns = `repeat(${size}, ${500/size}px)`;
-  container.style.gridAutoRows = `minmax(${100/size}px, auto)`;
-
 let clear = document.querySelector('#clear');
 clear.addEventListener('click',() => {
     location.reload();
